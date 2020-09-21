07978b659c8160d2af040f41b0adf322c7b51791
Emilio Berti <emilio.berti@bios.au.dk>
2020-09-21 17:41:16 +0200

    Figure 1 and 2 completed

--- Code/R/Contrast_figure.R
+++ Code/R/Contrast_figure.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(magrittr)
 library(circlize)
 library(see)
+library(gridExtra)
+library(gtable)
+
 
 rename_tl2 <- function(x){
   y <- switch (x,
@@ -46,23 +49,23 @@ contr %>%
                 width = 0) +
   xlab("") +
   ylab("Standardized difference") +
-  scale_x_discrete(labels = c("Rewilding \n vs \n No-extinction",
-                              "Rewilding \n vs \n Current",
-                              "Current \n vs \n No-extinction")) +
+  scale_x_discrete(labels = c("Rewilding - No-extinction",
+                              "Rewilding - Current",
+                              "Current - No-extinction")) +
   geom_hline(yintercept = 0, linetype = "dashed") +
-  facet_wrap(~Size, ncol = 2, scales = "free") +
+  scale_color_manual(values = as.vector(ggsci:::ggsci_db$startrek$uniform[c(2, 4)])) +
+  facet_wrap(~Size, ncol = 2, scales = "free_x") +
   coord_flip() +
   theme_classic() +
   theme(strip.background = element_blank(),
-        axis.text.y = element_text(hjust = 0.5,
-                                   size = 7),
+        text = element_text(size = 12),
         legend.title = element_blank(),
-        legend.position = "bottom") +
-  scale_color_manual(values = as.vector(ggsci:::ggsci_db$startrek$uniform[c(2, 4)]))
+        legend.text = element_text(size = 12),
+        legend.position = "bottom")
 
-ggsave("../../Manuscript/Figures/Contrasts_levels.png",
-       width = 6, 
-       height = 6)
+ggsave("../../Manuscript/Figures/Contrasts_levels.pdf", width = 6, height = 6)
+ggsave("../../Manuscript/Figures/Contrasts_levels.png", width = 6, height = 6)
+ggsave("../../Manuscript/Figures/Contrasts_levels.svg", width = 6, height = 6)
 
 # edges ------------------------------------------------------------------
 res <- read_csv("../../Results/edge_estimates.csv")
@@ -82,8 +85,16 @@ contr %>%
       rename_tl2()
     ans2 <- str_split(x, " -> ", simplify = TRUE)[2] %>% 
       rename_tl2()
-    ans <- paste(ans1, "\u279E", ans2)
+    ans <- paste(ans2, "\n \u2191 \n", ans1)
   }) %>% unlist()) %>% 
+  mutate(Edge = factor(Edge, levels = c("Megacarnivores \n ↑ \n Large carnivores",
+                                        "Megacarnivores \n ↑ \n Large herbivores",
+                                        "Megacarnivores \n ↑ \n Small carnivores",
+                                        "Megacarnivores \n ↑ \n Small herbivores",
+                                        "Large carnivores \n ↑ \n Large herbivores",
+                                        "Large carnivores \n ↑ \n Small carnivores",
+                                        "Large carnivores \n ↑ \n Small herbivores",
+                                        "Small carnivores \n ↑ \n Small herbivores"))) %>% 
   ggplot() +
   geom_point(aes(Contrast, -Difference, col = Area),
              position = position_dodge(width = 0.5)) +
@@ -93,48 +104,198 @@ contr %>%
                 width = 0) +
   xlab("") +
   ylab("Standardized difference") +
-  scale_x_discrete(labels = c("Rewilding \n vs \n No-extinction",
-                              "Rewilding \n vs \n Current",
-                              "Current \n vs \n No-extinction")) +
+  scale_x_discrete(labels = c("Rewilding - No-extinction",
+                              "Rewilding - Current",
+                              "Current - No-extinction")) +
   geom_hline(yintercept = 0, linetype = "dashed") +
-  facet_wrap(~Edge, ncol = 2, scales = "free") +
+  facet_wrap(~Edge, ncol = 2, scales = "free_x") +
   coord_flip() +
   theme_classic() +
+  scale_color_manual(values = as.vector(ggsci:::ggsci_db$startrek$uniform[c(2, 4)])) +
   theme(strip.background = element_blank(),
-        axis.text.y = element_text(hjust = 0.5,
-                                   size = 7),
+        text = element_text(size = 12),
         legend.title = element_blank(),
-        legend.position = "bottom") +
-  scale_color_manual(values = as.vector(ggsci:::ggsci_db$startrek$uniform[c(2, 4)]))
+        legend.text = element_text(size = 12),
+        legend.position = "bottom")
 
-ggsave("../../Manuscript/Figures/Contrasts_edges.png",
-       width = 6, 
-       height = 8)
+ggsave("../../Manuscript/Figures/Contrasts_edges.pdf", width = 6, height = 8)
+ggsave("../../Manuscript/Figures/Contrasts_edges.png", width = 6, height = 8)
+ggsave("../../Manuscript/Figures/Contrasts_edges.svg", width = 6, height = 8)
+
+fig_table <- tibble(
+  `Trophic level` = c("Megacarnivores", "Large carnivores", "Small carnivores",
+                      "Megaherbivores", "Large herbivores", "Small herbivores"),
+  `Size (kg)` = c("\u2265 100", "21.5-99", "< 21.5",
+                  "\u2265 1,000", "45-999", "< 45")
+)
+
+tm <- ttheme_minimal(core = list(fg_params = list(hjust = 0, x = 0)),
+                     rowhead = list(fg_params = list(hjust = 0, x = 0)))
+grid.arrange(tableGrob(fig_table, theme = tm, rows = NULL))
 
 # Chord-gram --------------------------------------------------------------
 lev <- read_csv("../../Results/level_estimates.csv")
 edge <- read_csv("../../Results/edge_estimates.csv")
 
-plot_chord <- function(d, scenario, area) {
-  d %<>% 
-    filter(Scenario == scenario,
-           Area == area) %>% 
+mycolor <- c(Megacarnivores = "tomato",
+             Megaherbivores = "dodgerblue",
+             `Large carnivores` = "orange", 
+             `Large herbivores` = "skyblue2",
+             `Small carnivores` = "lightgoldenrod2",
+             `Small herbivores` = "lightblue2")
+
+plot_chord <- function(d, area) {
+  d <- d %>% 
+    filter(Area == area) %>% 
+    group_by(Scenario) %>% 
+    group_split(keep = TRUE)
+  cu <- d[[1]]
+  pn <- d[[2]]
+  rw <- d[[3]]
+  cu %<>% 
     transmute(rowname = From,
               key = To,
-              value = log10(Estimate))
-  d %<>% 
+              value = log10(Estimate)) %>% 
     mutate(
       rowname = modify(rowname, function(x) {
         rename_tl2(x)
       }),
       key = modify(key, function(x) {
         rename_tl2(x)
-      }))
+      })) %>%
+    mutate(rowname = factor(rowname, levels = c("Megacarnivores",
+                                                "Large carnivores",
+                                                "Small carnivores",
+                                                "Small herbivores",
+                                                "Large herbivores")))
+  pn %<>% 
+    transmute(rowname = From,
+              key = To,
+              value = log10(Estimate)) %>% 
+    mutate(
+      rowname = modify(rowname, function(x) {
+        rename_tl2(x)
+      }),
+      key = modify(key, function(x) {
+        rename_tl2(x)
+      })) %>%
+    mutate(rowname = factor(rowname, levels = c("Megacarnivores",
+                                                "Large carnivores",
+                                                "Small carnivores",
+                                                "Small herbivores",
+                                                "Large herbivores")))
+  rw %<>% 
+    transmute(rowname = From,
+              key = To,
+              value = log10(Estimate)) %>% 
+    mutate(
+      rowname = modify(rowname, function(x) {
+        rename_tl2(x)
+      }),
+      key = modify(key, function(x) {
+        rename_tl2(x)
+      })) %>%
+    mutate(rowname = factor(rowname, levels = c("Megacarnivores",
+                                                "Large carnivores",
+                                                "Small carnivores",
+                                                "Small herbivores",
+                                                "Large herbivores")))
+  par(mfrow = c(1, 3),
+      mar = c(1, 1, 1, 1))
   circos.clear()
-  circos.par(start.degree = 90, 
-             gap.degree = 4, 
-             track.margin = c(-0.1, 0.1), 
+  circos.par(start.degree = 100, 
+             gap.after = c("Megacarnivores" = 5,
+                           "Large carnivores" = 5,
+                           "Small carnivores" = 20,
+                           "Small herbivores" = 5,
+                           "Large herbivores" = 20),
              points.overflow.warning = FALSE)
+  chordDiagram(pn,
+               order = levels(pn$rowname),
+               big.gap = 20,
+               small.gap = 5,
+               grid.col = mycolor,
+               transparency = 0.25,
+               directional = 1,
+               direction.type = c("diffHeight", "arrows"),
+               link.arr.type = "big.arrow",
+               annotationTrack = c("grid", "axis"), 
+               annotationTrackHeight = mm_h(5))
+  for(si in get.all.sector.index()) {
+    xlim = get.cell.meta.data("xlim", sector.index = si, track.index = 1)
+    ylim = get.cell.meta.data("ylim", sector.index = si, track.index = 1)
+    circos.text(mean(xlim), 
+                max(ylim) + 1.5,
+                si, 
+                sector.index = si, 
+                track.index = 1, 
+                facing = "bending.inside", 
+                niceFacing = TRUE, 
+                col = "black")
+  }
+  gap_cu <- calc_gap(pn, cu, big.gap = 20, small.gap = 5)
+  circos.clear()
+  circos.par(start.degree = 88, 
+             gap.after = c("Megacarnivores" = 5,
+                           "Large carnivores" = 5,
+                           "Small carnivores" = 20 + gap_cu / 2,
+                           "Small herbivores" = 5,
+                           "Large herbivores" = 20 + gap_cu / 2),
+             points.overflow.warning = FALSE)
+  chordDiagram(cu, 
+               order = levels(cu$rowname),
+               grid.col = mycolor,
+               transparency = 0.25,
+               directional = 1,
+               direction.type = c("diffHeight", "arrows"),
+               link.arr.type = "big.arrow",
+               annotationTrack = c("grid", "axis"), 
+               annotationTrackHeight = mm_h(5))
+  for(si in get.all.sector.index()) {
+    xlim = get.cell.meta.data("xlim", sector.index = si, track.index = 1)
+    ylim = get.cell.meta.data("ylim", sector.index = si, track.index = 1)
+    circos.text(mean(xlim), 
+                max(ylim) + 1.5,
+                si, 
+                sector.index = si, 
+                track.index = 1, 
+                facing = "bending.inside", 
+                niceFacing = TRUE, 
+                col = "black")
+  }
+  gap_rw <- calc_gap(pn, rw, big.gap = 20, small.gap = 5)
+  circos.clear()
+  circos.par(start.degree = 88, 
+             gap.after = c("Megacarnivores" = 5,
+                           "Large carnivores" = 5,
+                           "Small carnivores" = 20 + gap_rw / 2,
+                           "Small herbivores" = 5,
+                           "Large herbivores" = 20 + gap_rw / 2),
+             points.overflow.warning = FALSE)
+  chordDiagram(rw, 
+               order = levels(rw$rowname),
+               grid.col = mycolor,
+               transparency = 0.25,
+               directional = 1,
+               direction.type = c("diffHeight", "arrows"),
+               link.arr.type = "big.arrow",
+               annotationTrack = c("grid", "axis"), 
+               annotationTrackHeight = mm_h(5))
+  for(si in get.all.sector.index()) {
+    xlim = get.cell.meta.data("xlim", sector.index = si, track.index = 1)
+    ylim = get.cell.meta.data("ylim", sector.index = si, track.index = 1)
+    circos.text(mean(xlim), 
+                max(ylim) + 1.5,
+                si, 
+                sector.index = si, 
+                track.index = 1, 
+                facing = "bending.inside", 
+                niceFacing = TRUE, 
+                col = "black")
+  }
+  
+  
+  
   par(mar = rep(0, 4))
   chordDiagram(
     x = d, 
@@ -162,25 +323,44 @@ plot_chord <- function(d, scenario, area) {
         y = 3.2, 
         labels = sector.index, 
         facing = "bending", 
-        cex = 0.8
+        cex = 1.5
       )
       # Add graduation on axis
       circos.axis(
         h = "top", 
-        major.at = seq(from = 0, to = xlim[2], by = ifelse(test = xlim[2]>10, yes = 2, no = 1)), 
+        major.at = seq(from = 0, to = xlim[2], 
+                       by = ifelse(test = xlim[2] > 10, yes = 2, no = 1)), 
         minor.ticks = 1, 
         major.tick.percentage = 0.5,
-        labels.niceFacing = FALSE)
+        labels.niceFacing = FALSE,
+        labels.cex = 1)
     }
   )
-  return(d)
 }
+
+# data <- edge %>% 
+#   filter(Scenario == "PN",
+#          Area == "Protected area") %>% 
+#   pivot_wider(names_from = To, values_from = Mean) %>% 
+#   select(From, 9:11) %>% 
+#   mutate_all("replace_na", 0) %>% 
+#   mutate(From = factor(From))
+
 
 mycolor <- c("#E1AF00", "#3B9AB2",
              "#EBCC2A", "#78B7C5",
              "#F21A00", "#00637c")
 
+pdf("../../Manuscript/Figures/chordplot.pdf", width = 8, height = 4)
+par(mfrow = c(1, 3))
 plot_chord(edge, "PN", "Protected area")
+text(0, 1, "No-extinction", font = 2, cex = 2)
+plot_chord(edge, "CU", "Protected area")
+text(0, 1, "Current", font = 2, cex = 2)
+plot_chord(edge, "RW", "Protected area")
+text(0, 1, "Rewilding", font = 2, cex = 2)
+dev.off()
+
 
 pn <- edge %>% 
   filter(Scenario == "PN",
@@ -276,25 +456,24 @@ d %>%
   ungroup() %>% 
   mutate(Jitter = modify(as.numeric(Scenario), function(x) {
     x - abs(rnorm(1, 0, 0.1))
-    })) %>% 
+  })) %>% 
   ggplot() + 
   geom_jitter(aes(Jitter, Proportion),
-             size = 1.5, 
-             height = 0.1, 
-             alpha = 0.25) +
+              size = 1.5, 
+              height = 0.1, 
+              alpha = 0.25) +
   # geom_jitter(aes(Scenario, Proportion),
   #             alpha = 0.25,
   #             width = 0.25,
   #             height = 0.1,
   #             size = 0.75) +
   geom_violinhalf(aes(Scenario, Proportion, fill = Size),
-              show.legend = FALSE,
-              alpha = 0.5,
-              #bw = 1,
-              scale = "width",
-              trim = FALSE,
-              #draw_quantiles = 0.5,
-              size = 0.5) +
+                  show.legend = FALSE,
+                  alpha = 0.5,
+                  scale = "width",
+                  trim = FALSE,
+                  #draw_quantiles = 0.5,
+                  size = 0.5) +
   xlab("") +
   ylab("Number of species") +
   scale_x_discrete(labels = c("No-extinction", "Current", "Rewilding")) +
@@ -303,9 +482,12 @@ d %>%
   theme_classic() +
   theme(strip.background = element_blank(),
         strip.text = element_text(hjust = 0.5),
-        text = element_text(size = 14),
-        strip.text.x = element_text(size = 12))
+        text = element_text(size = 16),
+        strip.text.x = element_text(size = 12),
+        axis.text.x = element_text(angle = 30,
+                                   vjust = 0.5))
 
+ggsave("../../Manuscript/Figures/raw_levels.pdf", width = 6.5, height = 8)
 ggsave("../../Manuscript/Figures/raw_levels.png", width = 6.5, height = 8)
 ggsave("../../Manuscript/Figures/raw_levels.svg", width = 6.5, height = 8)
 
